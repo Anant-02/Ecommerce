0d2c25ecb340a067dfd0883714808c8343638e34
Anant-02 <80309937+Anant-02@users.noreply.github.com>
2022-11-28 01:47:51 +0530

    redux store added

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -9,14 +9,15 @@ router.get("/", asyncHandler( async (req, res) => {
    res.json(products)
 }))
 
-router.get("/:id", async(req, res) => {
+router.get("/:id", asyncHandler( async(req, res) => {
     const product = await Product.findById(req.params.id) 
     if(product) {
         res.json(product)
     }
     else {
-        res.status(404).json({message: "Product not found."})
+        res.status(404)
+        throw new Error('Product not Found')
     }
-})
+}))
 
 export default router;
